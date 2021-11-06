0a6625e18e2d8de4d0f37a526d92db2c0e87723e
Sun Lama <isamo.agverdiev@gmail.com>
2021-11-06 19:24:01 +0300

    feat: add 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, () => {});
+
+fs.readdir(path.join(__dirname, 'files-copy'), { withFileTypes: true }, (err, files) => {
+  for (let file of files) {
+    fs.unlink(path.join(__dirname, 'files-copy', file.name), err => {});
+  }
+
+  fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
+    for (let file of files) {
+      fs.copyFile(
+        path.join(__dirname, 'files', file.name),
+        path.join(__dirname, 'files-copy', file.name),
+        () => {}
+      );
+    }
+  });
+});
